440ffa4778a1d46691e4ba9c12a6d954db54bda8
JuanAntonioMN <106030895+JuanAntonioMN@users.noreply.github.com>
2023-03-12 00:16:57 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include "Equipos.h"
-
+/*
+	Name: SIMULADOR DE PARTIDOS DE FUTBOL
+	Copyright: JAMN
+	Author: Juan Antonio Maldonado Nepomuceno
+	Date: 12/03/23 00:15
+	Description: SIMULADOR DE PARTIDOS DE FUTBOL
+*/
 int main(int argc, char** argv) {
 	Equipos obj;
 	obj.Menu();
